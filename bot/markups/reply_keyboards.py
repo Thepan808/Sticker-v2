20f493c706c32e20c676b1ad866f93aa02e9cfca
The Vampiro Elegant <streetdemonic003@gmail.com>
2022-12-17 16:50:55 -0300

    Update reply_keyboards.py

--- bot/markups/reply_keyboards.py
+++ bot/markups/reply_keyboards.py
@@ -9,6 +9,6 @@ class Keyboard:
     def from_list(items, add_back_button=False):
         items_copy = [i for i in items]  # we need to create a copy of this list otherwise the back button might be
         if add_back_button:
-            items_copy.append('GO BACK')
+            items_copy.append('VOLTAR')
 
         return ReplyKeyboardMarkup([[i] for i in items_copy], resize_keyboard=True)
