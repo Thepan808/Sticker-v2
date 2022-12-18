746b47a2e020c8a7461af4c91b889fef5deae7d0
The Vampiro Elegant <streetdemonic003@gmail.com>
2022-12-17 21:29:58 -0300

    Update inline_keyboards.py

--- bot/markups/inline_keyboards.py
+++ bot/markups/inline_keyboards.py
@@ -11,15 +11,15 @@ class InlineKeyboard:
     @staticmethod
     def pack_type_switch(sticker_type: int = StickerType.STATIC):
         static_button = InlineKeyboardButton(
-            '{} static'.format('✅' if sticker_type == StickerType.STATIC else '☑️'),
+            '{} estático'.format('✅' if sticker_type == StickerType.STATIC else '☑️'),
             callback_data=f'packtype:{StickerType.STATIC}'
         )
         animated_button = InlineKeyboardButton(
-            '{} animated'.format('✅' if sticker_type == StickerType.ANIMATED else '☑️'),
+            '{} animado'.format('✅' if sticker_type == StickerType.ANIMATED else '☑️'),
             callback_data=f'packtype:{StickerType.ANIMATED}'
         )
         video_button = InlineKeyboardButton(
-            '{} video'.format('✅' if sticker_type == StickerType.VIDEO else '☑️'),
+            '{} vídeo'.format('✅' if sticker_type == StickerType.VIDEO else '☑️'),
             callback_data=f'packtype:{StickerType.VIDEO}'
         )
 
